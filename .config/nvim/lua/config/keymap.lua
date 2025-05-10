cc42021b402ff8ab28b4d5a12e1399cdd81ea0c2
stks56 <t.suenaga428@gmail.com>
2025-05-11 02:40:23 +0900

    nvim: change keymap

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -1,11 +1,2 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "i", "k")
-vim.keymap.set("n", "k", "j")
-vim.keymap.set("n", "j", "h")
-vim.keymap.set("n", "h", "i")
-
-vim.keymap.set("v", "i", "k")
-vim.keymap.set("v", "k", "j")
-vim.keymap.set("v", "j", "h")
-vim.keymap.set("v", "h", "i")
