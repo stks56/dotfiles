d2e535adb49914c2c9507bfddd6ef9b89427dbb8
stks56 <t.suenaga428@gmail.com>
2025-05-12 06:52:32 +0900

    nvim: add oil keymap

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,13 +1,29 @@
- return {
-  'stevearc/oil.nvim',
-  -- Optional dependencies
-  -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-  lazy = false,
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {
+return {
+	"stevearc/oil.nvim",
+	-- Optional dependencies
+	-- dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+	lazy = false,
+	keys = {
+		{
+			"<leader>dw",
+			function()
+				require("oil").open(vim.fn.getcwd())
+			end,
+			desc = "Oil: Open working directory",
+		},
+		{
+			"<leader>dc",
+			function()
+				require("oil").open()
+			end,
+			desc = "Oil: Open current directory",
+		},
+	},
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {
 		default_file_explorer = true,
 		use_default_keymaps = true,
 		view_options = {
