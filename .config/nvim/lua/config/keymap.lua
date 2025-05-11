6e2b4da3173527026df08436bf9778cf250304d0
stks56 <t.suenaga428@gmail.com>
2025-05-12 00:55:42 +0900

    Revert "nvim: change keymap"
    This reverts commit cc42021b402ff8ab28b4d5a12e1399cdd81ea0c2.

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -1,2 +1,11 @@
 vim.g.mapleader = " "
 
+vim.keymap.set("n", "i", "k")
+vim.keymap.set("n", "k", "j")
+vim.keymap.set("n", "j", "h")
+vim.keymap.set("n", "h", "i")
+
+vim.keymap.set("v", "i", "k")
+vim.keymap.set("v", "k", "j")
+vim.keymap.set("v", "j", "h")
+vim.keymap.set("v", "h", "i")
