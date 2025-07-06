6852b55d031e8538a3772461c6f842c537ef8f70
stks56 <t.suenaga428@gmail.com>
2025-07-07 01:33:18 +0900

    wezterm: update config

--- .wezterm.lua
+++ .wezterm.lua
@@ -12,182 +12,118 @@ config.inactive_pane_hsb = { brightness = 0.5 }
 config.window_decorations = "RESIZE"
 
 -- Key binding
-local mac = wezterm.target_triple:find("darwin")
-local linux = wezterm.target_triple:find("-linux-")
-
-if mac then
-	config.keys = {
-		{
-			key = "f",
-			mods = "CMD",
-			action = wezterm.action.SendKey({ key = "f", mods = "CTRL" }),
-		},
-		{
-			key = "b",
-			mods = "CMD",
-			action = wezterm.action.SendKey({ key = "b", mods = "CTRL" }),
-		},
-		{
-			key = "n",
-			mods = "CMD",
-			action = wezterm.action.SendKey({ key = "n", mods = "CTRL" }),
-		},
-		{
-			key = "p",
-			mods = "CMD",
-			action = wezterm.action.SendKey({ key = "p", mods = "CTRL" }),
-		},
-		{
-			key = "d",
-			mods = "CMD",
-			action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-		},
-		{
-			key = "D",
-			mods = "CMD",
-			action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-		},
-		{
-			key = "w",
-			mods = "CMD",
-			action = wezterm.action.CloseCurrentPane({ confirm = true }),
-		},
-		{
-			key = "Tab",
-			mods = "CTRL",
-			action = wezterm.action.ActivatePaneDirection("Next"),
-		},
-		{
-			key = "q",
-			mods = "CMD",
-			action = wezterm.action({ SendString = "\x00" }),
-		},
-		{
-			key = "e",
-			mods = "CMD",
-			action = wezterm.action({ SendString = "\x04" }),
-		},
-		{
-			key = "Backspace",
-			mods = "CMD",
-			action = wezterm.action({ SendString = "\x16" }), -- backward-kill-word
-		},
-		{
-			key = "Backspace",
-			mods = "SHIFT|CMD",
-			action = wezterm.action({ SendString = "\x14" }), -- backward-kill-line
-		},
-		{
-			key = "LeftArrow",
-			mods = "ALT",
-			action = wezterm.action({ SendString = "\x0bb" }), -- backward-word
-		},
-		{
-			key = "RightArrow",
-			mods = "ALT",
-			action = wezterm.action({ SendString = "\x0bf" }), -- backward-word
-		},
-		{
-			key = "LeftArrow",
-			mods = "CMD",
-			action = wezterm.action({ SendString = "\x00" }), -- beginning-of-line
-		},
-		{
-			key = "RightArrow",
-			mods = "CMD",
-			action = wezterm.action({ SendString = "\x04" }), -- end-of-line
-		},
-		{
-			key = "C",
-			mods = "CMD|SHIFT",
-			action = wezterm.action({ SendString = "\x03" }),
-		},
-		-- {key="LeftArrow", mods="CMD|SHIFT", action=wezterm.action{SendKey={key="Home"}}},
-		-- {key="LeftArrow", mods="SHIFT", action="SelectTextAtCursorLeft"},
-		-- {key="RightArrow", mods="SHIFT", action="SelectTextAtCursorRight"}
-	}
-elseif linux then
-	config.keys = {
-		{
-			key = "d",
-			mods = "CTRL",
-			action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-		},
-		{
-			key = "D",
-			mods = "CTRL",
-			action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-		},
-		{
-			key = "w",
-			mods = "CTRL",
-			action = wezterm.action.CloseCurrentPane({ confirm = true }),
-		},
-		{
-			key = "Tab",
-			mods = "CTRL",
-			action = wezterm.action.ActivatePaneDirection("Next"),
-		},
-		{
-			key = "q",
-			mods = "CTRL",
-			action = wezterm.action({ SendString = "\x00" }),
-		},
-		{
-			key = "e",
-			mods = "CTRL",
-			action = wezterm.action({ SendString = "\x04" }),
-		},
-		{
-			key = "Backspace",
-			mods = "CTRL",
-			action = wezterm.action({ SendString = "\x16" }), -- backward-kill-word
-		},
-		{
-			key = "Backspace",
-			mods = "SHIFT|CTRL",
-			action = wezterm.action({ SendString = "\x14" }), -- backward-kill-line
-		},
-		{
-			key = "LeftArrow",
-			mods = "ALT",
-			action = wezterm.action({ SendString = "\x0bb" }), -- backward-word
-		},
-		{
-			key = "RightArrow",
-			mods = "ALT",
-			action = wezterm.action({ SendString = "\x0bf" }), -- backward-word
-		},
-		{
-			key = "LeftArrow",
-			mods = "CTRL",
-			action = wezterm.action({ SendString = "\x00" }), -- beginning-of-line
-		},
-		{
-			key = "RightArrow",
-			mods = "CTRL",
-			action = wezterm.action({ SendString = "\x04" }), -- end-of-line
-		},
-		{
-			key = "c",
-			mods = "CTRL",
-			action = wezterm.action.CopyTo("Clipboard"),
-		},
-		{
-			key = "v",
-			mods = "CTRL",
-			action = wezterm.action.PasteFrom("Clipboard"),
-		},
-		{
-			key = "C",
-			mods = "CTRL|SHIFT",
-			action = wezterm.action({ SendString = "\x03" }),
-		},
-		-- {key="LeftArrow", mods="CMD|SHIFT", action=wezterm.action{SendKey={key="Home"}}},
-		-- {key="LeftArrow", mods="SHIFT", action="SelectTextAtCursorLeft"},
-		-- {key="RightArrow", mods="SHIFT", action="SelectTextAtCursorRight"}
-	}
+local mainmod = "CTRL"
+if wezterm.target_triple:find("darwin") then
+	mainmod = "CMD"
 end
+
+wezterm.macos_forward_to_ime_modifier_mask = "CMD"
+
+config.keys = {
+	{
+		key = "f",
+		mods = mainmod,
+		action = wezterm.action({ SendString = "\x06" }), -- forward-char
+	},
+	{
+		key = "b",
+		mods = mainmod,
+		action = wezterm.action({ SendString = "\x02" }), -- backward-char
+	},
+	{
+		key = "p",
+		mods = mainmod,
+		action = wezterm.action({ SendString = "\x10" }), -- previous-line
+	},
+	{
+		key = "n",
+		mods = mainmod,
+		action = wezterm.action({ SendString = "\x0E" }), -- next-line
+	},
+	{
+		key = "a",
+		mods = mainmod,
+		action = wezterm.action({ SendString = "\x01" }), -- beginning-of-line
+	},
+	{
+		key = "e",
+		mods = mainmod,
+		action = wezterm.action({ SendString = "\x05" }), -- end-of-line
+	},
+	{
+		key = "f",
+		mods = mainmod .. "|SHIFT",
+		action = wezterm.action({ SendString = "\x0bf" }), -- forward-word
+	},
+	{
+		key = "b",
+		mods = mainmod .. "|SHIFT",
+		action = wezterm.action({ SendString = "\x0bb" }), -- backward-word
+	},
+	{
+		key = "l",
+		mods = mainmod,
+		action = wezterm.action.ClearScrollback("ScrollbackOnly"),
+	},
+	{
+		key = "d",
+		mods = mainmod,
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "D",
+		mods = mainmod,
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "w",
+		mods = mainmod,
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+	{
+		key = "Tab",
+		mods = mainmod,
+		action = wezterm.action.ActivatePaneDirection("Next"),
+	},
+	{
+		key = "q",
+		mods = mainmod,
+		action = wezterm.action({ SendString = "\x00" }),
+	},
+	{
+		key = "e",
+		mods = mainmod,
+		action = wezterm.action({ SendString = "\x04" }),
+	},
+	{
+		key = "Backspace",
+		mods = mainmod,
+		action = wezterm.action({ SendString = "\x16" }), -- backward-kill-word
+	},
+	{
+		key = "Backspace",
+		mods = mainmod .. "|SHIFT",
+		action = wezterm.action({ SendString = "\x14" }), -- backward-kill-line
+	},
+	{
+		key = "c",
+		mods = mainmod,
+		action = wezterm.action.CopyTo("Clipboard"),
+	},
+	{
+		key = "v",
+		mods = mainmod,
+		action = wezterm.action.PasteFrom("Clipboard"),
+	},
+	{
+		key = "C",
+		mods = mainmod .. "|SHIFT",
+		action = wezterm.action({ SendString = "\x03" }),
+	},
+	-- {key="LeftArrow", mods="CMD|SHIFT", action=wezterm.action{SendKey={key="Home"}}},
+	-- {key="LeftArrow", mods="SHIFT", action="SelectTextAtCursorLeft"},
+	-- {key="RightArrow", mods="SHIFT", action="SelectTextAtCursorRight"}
+}
 
 -- End configuration
 return config
