b21107d98c124f3deb8a854688e0498bf9055653
stks56 <t.suenaga428@gmail.com>
2025-07-16 21:27:59 +0900

    wezterm: update

--- .config/wezterm/darwin/keybinds.lua
+++ .config/wezterm/darwin/keybinds.lua
@@ -4,6 +4,12 @@ local act = wezterm.action
 local M = {}
 
 function M.apply_to_config(config)
+	config.leader = {
+		key = "d",
+		mods = "CTRL",
+		timeout_milliseconds = 1000,
+	}
+
 	config.keys = {
 		{
 			key = "Tab",
@@ -16,19 +22,59 @@ function M.apply_to_config(config)
 			action = act({ SendString = "\x00" }),
 		},
 		{
-			key = "w",
-			mods = "CTRL",
-			action = act.CloseCurrentTab({ confirm = true }),
-		},
-		{
 			key = "t",
 			mods = "CTRL",
 			action = act.SpawnTab("CurrentPaneDomain"),
 		},
 		{
+			key = "q",
+			mods = "CTRL",
+			action = act({ SendString = "\x00" }),
+		},
+		{
+			key = '"',
+			mods = "LEADER",
+			action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+		},
+		{
+			key = "%",
+			mods = "LEADER",
+			action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+		},
+		{
+			key = "j",
+			mods = "LEADER",
+			action = act.AdjustPaneSize({ "Left", 5 }),
+		},
+		{
+			key = "k",
+			mods = "LEADER",
+			action = act.AdjustPaneSize({ "Down", 5 }),
+		},
+		{
+			key = "i",
+			mods = "LEADER",
+			action = act.AdjustPaneSize({ "Up", 5 }),
+		},
+		{
+			key = "l",
+			mods = "LEADER",
+			action = act.AdjustPaneSize({ "Right", 5 }),
+		},
+		{
+			key = "q",
+			mods = "CTRL",
+			action = act({ SendString = "\x00" }),
+		},
+		{
 			key = "m",
 			mods = "CTRL",
 			action = act.PaneSelect,
+		},
+		{
+			key = "{",
+			mods = "CTRL",
+			action = act.PaneSelect({ mode = "SwapWithActive" }),
 		},
 		{
 			key = "s",
