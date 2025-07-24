166fcd48b0d4d759be05fa03c75f13eba340fd6e
stks56 <t.suenaga428@gmail.com>
2025-07-25 00:53:50 +0900

    nvim: install neoscroll

---
+++ .config/nvim/lua/plugins/neoscroll.lua
@@ -0,0 +1,43 @@
+return {
+	"karb94/neoscroll.nvim",
+	config = function()
+		require("neoscroll").setup({})
+
+		neoscroll = require("neoscroll")
+
+		local keymap = {
+			["<C-u>"] = function()
+				neoscroll.ctrl_u({ duration = 200 })
+			end,
+			["<C-d>"] = function()
+				neoscroll.ctrl_d({ duration = 200 })
+			end,
+			["<C-b>"] = function()
+				neoscroll.ctrl_b({ duration = 300 })
+			end,
+			["<C-f>"] = function()
+				neoscroll.ctrl_f({ duration = 300 })
+			end,
+			["<C-y>"] = function()
+				neoscroll.scroll(-0.1, { move_cursor = false, duration = 100 })
+			end,
+			["<C-e>"] = function()
+				neoscroll.scroll(0.1, { move_cursor = false, duration = 100 })
+			end,
+			["zt"] = function()
+				neoscroll.zt({ half_win_duration = 250 })
+			end,
+			["zz"] = function()
+				neoscroll.zz({ half_win_duration = 250 })
+			end,
+			["zb"] = function()
+				neoscroll.zb({ half_win_duration = 250 })
+			end,
+		}
+
+		local modes = { "n", "v", "x" }
+		for key, func in pairs(keymap) do
+			vim.keymap.set(modes, key, func)
+		end
+	end,
+}
