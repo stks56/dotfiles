78d9a920d1e4bd1ffb4ae837bcacecc090d580e0
stks56 <t.suenaga428@gmail.com>
2025-07-28 02:24:05 +0900

    nvim: update testing

---
+++ .config/nvim/lua/plugins/testing.lua
@@ -0,0 +1,45 @@
+return {
+	"nvim-neotest/neotest",
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"nvim-lua/plenary.nvim",
+		"antoinemadec/FixCursorHold.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		"fredrikaverpil/neotest-golang",
+	},
+	config = function()
+		---@diagnostic disable-next-line: missing-fields
+		require("neotest").setup({
+			adapters = {
+				require("neotest-golang")({
+					go_test_args = { "-v", "-race", "-count=1" },
+					dap_go_enabled = false,
+				}),
+			},
+		})
+	end,
+	keys = {
+		{
+			"<leader>tt",
+			function()
+				require("neotest").run.run()
+			end,
+			desc = "Run all tests (neotest)",
+		},
+		{
+			"<leader>to",
+			function()
+				require("neotest").output_panel.toggle()
+			end,
+			desc = "Toggle test output panel",
+		},
+		{
+			"<leader>ts",
+			function()
+				require("neotest").run.stop()
+			end,
+			desc = "Stop running test",
+		},
+	},
+	version = "*",
+}
