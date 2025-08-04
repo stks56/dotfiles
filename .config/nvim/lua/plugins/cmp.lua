cd799cd045021d90cac849751fa542be8d6c9dde
stks56 <t.suenaga428@gmail.com>
2025-08-04 19:26:01 +0900

    nvim: update cmp

---
+++ .config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,50 @@
+return {
+	{
+		"saghen/blink.cmp",
+		opts = {
+			sources = {
+				default = {
+					"lsp",
+					"lazydev",
+					"path",
+					"snippets",
+					-- "buffer",
+				},
+				providers = {
+					lazydev = {
+						name = "LazyDev",
+						module = "lazydev.integrations.blink",
+						score_offset = 100,
+					},
+				},
+			},
+			completion = {
+				list = {
+					selection = {
+						preselect = false,
+					},
+				},
+				documentation = {
+					auto_show = true,
+				},
+			},
+			keymap = {
+				["<CR>"] = { "select_and_accept", "fallback" },
+			},
+		},
+		version = "*",
+	},
+	{
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		opts = {
+			library = {
+				-- See the configuration section for more details
+				-- Load luvit types when the `vim.uv` word is found
+				"lazy.nvim",
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
+		},
+		version = "*",
+	},
+}
