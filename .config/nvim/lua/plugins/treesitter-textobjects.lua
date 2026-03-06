e2099bff99a049fd720265555b0df32b01519389
stks56 <t.suenaga428@gmail.com>
2026-03-06 10:55:35 +0900

    nvim: configure ts textobject

--- .config/nvim/lua/plugins/treesitter-textobjects.lua
+++ .config/nvim/lua/plugins/treesitter-textobjects.lua
@@ -1,5 +1,30 @@
 return {
 	"nvim-treesitter/nvim-treesitter-textobjects",
-	branch = "main",
 	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	branch = "main",
+	init = function()
+		vim.g.no_plugin_maps = true
+	end,
+	keymaps = {
+		{
+			"af",
+			"<cmd>lua require('nvim-treesitter.textobjects.select').select_textobject('af')<CR>",
+			desc = "Select around function",
+		},
+		{
+			"if",
+			"<cmd>lua require('nvim-treesitter.textobjects.select').select_textobject('if')<CR>",
+			desc = "Select inside function",
+		},
+		{
+			"ac",
+			"<cmd>lua require('nvim-treesitter.textobjects.select').select_textobject('ac')<CR>",
+			desc = "Select around class",
+		},
+		{
+			"ic",
+			"<cmd>lua require('nvim-treesitter.textobjects.select').select_textobject('ic')<CR>",
+			desc = "Select inside class",
+		},
+	},
 }
