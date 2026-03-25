453576eb27b8beb8a4b41d45554bd2ffe7432947
stks56 <t.suenaga428@gmail.com>
2026-03-25 19:43:36 +0900

    nvim: fix textobjects settings

--- .config/nvim/lua/plugins/treesitter-textobjects.lua
+++ .config/nvim/lua/plugins/treesitter-textobjects.lua
@@ -5,20 +5,33 @@ return {
 	init = function()
 		vim.g.no_plugin_maps = true
 	end,
-	opts = {
-		textobjects = {
+	config = function()
+		require("nvim-treesitter-textobjects").setup({
 			select = {
-				enable = true,
 				lookahead = true,
-				keymaps = {
-					["af"] = "@function.outer",
-					["if"] = "@function.inner",
-					["ac"] = "@class.outer",
-					["ic"] = "@class.inner",
-					["aa"] = "@parameter.outer",
-					["ia"] = "@parameter.inner",
-				},
 			},
-		},
-	},
+		})
+
+		local select = require("nvim-treesitter-textobjects.select")
+		local map = vim.keymap.set
+
+		map({ "x", "o" }, "af", function()
+			select.select_textobject("@function.outer", "textobjects")
+		end)
+		map({ "x", "o" }, "if", function()
+			select.select_textobject("@function.inner", "textobjects")
+		end)
+		map({ "x", "o" }, "ac", function()
+			select.select_textobject("@class.outer", "textobjects")
+		end)
+		map({ "x", "o" }, "ic", function()
+			select.select_textobject("@class.inner", "textobjects")
+		end)
+		map({ "x", "o" }, "aa", function()
+			select.select_textobject("@parameter.outer", "textobjects")
+		end)
+		map({ "x", "o" }, "ia", function()
+			select.select_textobject("@parameter.inner", "textobjects")
+		end)
+	end,
 }
