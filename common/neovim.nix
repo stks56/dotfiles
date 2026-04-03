0e2e120612877f54e98b9a7fde908e87adf4afa6
stks56 <t.suenaga428@gmail.com>
2026-04-03 13:58:45 +0900

    fix lazy-lock on git

--- common/neovim.nix
+++ common/neovim.nix
@@ -1,10 +1,14 @@
+{ config, ... }:
+
 {
   programs.neovim.enable = true;
 
   home.file = {
     ".config/nvim" = {
-      source = ../.config/nvim;
+      source = builtins.filterSource (path: _: baseNameOf path != "lazy-lock.json") ../.config/nvim;
       recursive = true;
     };
+    ".config/nvim/lazy-lock.json".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/.config/nvim/lazy-lock.json";
   };
 }
