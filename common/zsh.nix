18606ddd241127502d38a1b160cd2e3b36e4304d
stks56 <t.suenaga428@gmail.com>
2026-04-03 14:07:33 +0900

    fix zsh vim keymap

--- common/zsh.nix
+++ common/zsh.nix
@@ -7,6 +7,7 @@
       highlight = "fg=6";
     };
     syntaxHighlighting.enable = true;
+    defaultKeymap = "emacs";
 
     zsh-abbr = {
       enable = true;
@@ -40,6 +41,9 @@
     atuin = {
       enable = true;
       enableZshIntegration = true;
+      flags = [
+        "--disable-up-arrow"
+      ];
       settings = {
         keymap_mode = "emacs";
       };
