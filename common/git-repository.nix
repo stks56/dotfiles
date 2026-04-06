30003d9f0ee922c919400db8a5db17622c8972f6
stks56 <t.suenaga428@gmail.com>
2026-04-06 13:48:55 +0900

    fix: git repository path

--- common/git-repository.nix
+++ common/git-repository.nix
@@ -1,11 +1,11 @@
-{ ... }:
+{ config, ... }:
 
 {
   services.git-sync = {
     enable = true;
     repositories = {
       cortex = {
-        path = "/home/nixos/works/cortex";
+        path = "${config.home.homeDirectory}/cortex";
         uri = "https://github.com/stks56/cortex.git";
         interval = 60;
       };
