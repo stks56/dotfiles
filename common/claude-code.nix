afb74047a375793a968b3205267c3845a4b68475
stks56 <t.suenaga428@gmail.com>
2026-04-08 10:35:56 +0900

    fix disable claude setting

--- common/claude-code.nix
+++ common/claude-code.nix
@@ -10,12 +10,12 @@ in
     pkgs.claude-code
   ];
 
-  home.file = {
-    ".claude/settings.json".source = mkLink "settings.json";
-    ".claude/statusline.sh".source = mkLink "statusline.sh";
-    ".claude/CLAUDE.md".source = mkLink "CLAUDE.md";
-    ".claude/RTK.md".source = mkLink "RTK.md";
-    ".claude/hooks".source = mkLink "hooks";
-    ".claude/skills".source = mkLink "skills";
-  };
+  # home.file = {
+  #   ".claude/settings.json".source = mkLink "settings.json";
+  #   ".claude/statusline.sh".source = mkLink "statusline.sh";
+  #   ".claude/CLAUDE.md".source = mkLink "CLAUDE.md";
+  #   ".claude/RTK.md".source = mkLink "RTK.md";
+  #   ".claude/hooks".source = mkLink "hooks";
+  #   ".claude/skills".source = mkLink "skills";
+  # };
 }
