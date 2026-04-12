08ae5d60fb1bf52b5abac0ed61c628f5f88f3e0f
stks56 <t.suenaga428@gmail.com>
2026-04-12 14:20:49 +0900

    add waybar setting

--- hosts/orpar/hyprland.nix
+++ hosts/orpar/hyprland.nix
@@ -3,6 +3,12 @@
 {
   wayland.windowManager.hyprland = {
     enable = true;
+    systemd.enable = false;
     extraConfig = "source = ${../../.config/hypr/hyprland.conf}";
   };
+
+  programs.waybar.enable = true;
+
+  xdg.configFile."waybar/config.jsonc".source = ../../.config/waybar/config.jsonc;
+  xdg.configFile."waybar/style.css".source = ../../.config/waybar/style.css;
 }
