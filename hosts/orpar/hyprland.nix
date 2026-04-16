4651343c67e5104d4f1a8a48e53ced690d22c6b0
stks56 <t.suenaga428@gmail.com>
2026-04-17 01:47:25 +0900

    waypaper: add settings and wallpapers

--- hosts/orpar/hyprland.nix
+++ hosts/orpar/hyprland.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, pkgs, ... }:
 
 {
   wayland.windowManager.hyprland = {
@@ -11,4 +11,9 @@
 
   xdg.configFile."waybar/config.jsonc".source = ../../.config/waybar/config.jsonc;
   xdg.configFile."waybar/style.css".source = ../../.config/waybar/style.css;
+  xdg.configFile."waypaper/config.ini".source = pkgs.replaceVars ../../.config/waypaper/config.ini {
+    homeDirectory = config.home.homeDirectory;
+  };
+  xdg.configFile."waypaper/style.css".source = ../../.config/waypaper/style.css;
+  xdg.configFile."hypr/wallpapers".source = ../../.config/hypr/wallpapers;
 }
